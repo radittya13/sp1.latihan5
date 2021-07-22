3ae3f09a75b5369afd7be4b294da81df5ac7a56e
radittya13 <86655614+radittya13@users.noreply.github.com>
2021-07-22 12:55:52 +0700

    Add files via upload

---
+++ radittya.js
@@ -0,0 +1,32 @@
+//var xx = document.getElementById("text");
+	//id a1 di ambil dan di tampung dalam var button1
+	//var button1 = document.getElementById("a1");
+	//id a2 di ambil dan di tampung dalam var button2
+	//var button2 = document.getElementById("a2");
+ 
+	//untuk merubah warna menjadi black
+	//button1.addEventListener("click", function () {xx.style.color="navy";} , false);
+	//untuk merubah warna menjadi black
+	//button2.addEventListener("click", function () {xx.style.color="gold";} , false);
+
+
+
+
+
+
+	var nilai = prompt("inputkan nilai akhir");
+	var nama  = prompt("input nama")
+
+
+	if(nilai>=70) {
+		document.write(nama  + " selamat anda lulus dengan nilai: " + nilai )
+	}else{
+		document.write(" mohon maaf "+ nama + " anda tidak lulus, karena nilai anda :" + nilai )
+	}
+
+
+	
+	 
+
+
+
